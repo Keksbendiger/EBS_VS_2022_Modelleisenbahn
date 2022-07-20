cf63394558ce6c4fa1d8f0eba5197dd570344197
Keksbendiger <keksbendiger@gmail.com>
2022-07-20 14:04:00 +0200

    made gui setup blocking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,49 @@ import mqtt.MqttClient;
 
 public class Main {
     public static void main(String[] args) {
+        GuiHandler guiHandler = GuiHandler.getInstance();
+
+        //region GUI
+        /* Set the Nimbus look and feel */
+        //region Look and feel setting code
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //endregion Look and feel setting code
+
+        /* Create and display the form */
+        guiHandler.setGui(new GUI());
+        guiHandler.getGui().setVisible(true);
+
+        guiHandler.setTrackSectionUsed(ETrackSection.I, true, "ICE");
+        guiHandler.setTrackSectionUsed(ETrackSection.C, true, "Güterzug A");
+        guiHandler.setTrackSectionUsed(ETrackSection.G, true, "Güterzug B");
+
+        guiHandler.setSwitchDirection(ETrackSwitch.ONE, ETrackSection.G);
+        guiHandler.setSwitchDirection(ETrackSwitch.TWO, ETrackSection.G);
+        guiHandler.setSwitchDirection(ETrackSwitch.THREE, ETrackSection.D);
+        guiHandler.setSwitchDirection(ETrackSwitch.FOUR, ETrackSection.D);
+        guiHandler.setSwitchDirection(ETrackSwitch.FIVE, ETrackSection.I);
+        guiHandler.setSwitchDirection(ETrackSwitch.SIX, ETrackSection.I);
+
+        //endregion GUI
+
         //region INIT Track System
         new TrackSection(ETrackSection.A, 3);
         new TrackSection(ETrackSection.B, 0);
@@ -56,53 +99,6 @@ public class Main {
         new Train("Güterzug rot", 1, 6, Train.TrainDirection.COUNTERCLOCKWISE);
         TrackSection.get(ETrackSection.G).block(Train.get("Güterzug rot"));
         //endregion INIT Trains
-
-        GuiHandler guiHandler = GuiHandler.getInstance();
-
-        //region GUI
-        /* Set the Nimbus look and feel */
-        //region Look and feel setting code
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //endregion Look and feel setting code
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                guiHandler.setGui(new GUI());
-                guiHandler.getGui().setVisible(true);
-
-                guiHandler.setTrackSectionUsed(ETrackSection.I, true, "ICE");
-                guiHandler.setTrackSectionUsed(ETrackSection.C, true, "Güterzug A");
-                guiHandler.setTrackSectionUsed(ETrackSection.G, true, "Güterzug B");
-
-                guiHandler.setSwitchDirection(ETrackSwitch.ONE, ETrackSection.G);
-                guiHandler.setSwitchDirection(ETrackSwitch.TWO, ETrackSection.G);
-                guiHandler.setSwitchDirection(ETrackSwitch.THREE, ETrackSection.D);
-                guiHandler.setSwitchDirection(ETrackSwitch.FOUR, ETrackSection.D);
-                guiHandler.setSwitchDirection(ETrackSwitch.FIVE, ETrackSection.I);
-                guiHandler.setSwitchDirection(ETrackSwitch.SIX, ETrackSection.I);
-            }
-        });
-
-        //endregion GUI
 
         //region GUI Data Faker
         try {
